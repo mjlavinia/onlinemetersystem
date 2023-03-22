f2d40e96b3c95ad3d544c7f0676a8f8af1edb050
joy <you@example.com>
2023-03-22 22:51:05 +0800

    admin changes

--- Client/admin.py
+++ Client/admin.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from .models import BillingInfo,ClientInfo,RealTimeBill,Billing
 
 admin.site.register(BillingInfo)
-admin.site.register(RealTimeBill)
-admin.site.register(ClientInfo)
+#admin.site.register(RealTimeBill)
+
 admin.site.register(Billing)
 # Register your models here..re
+
+@admin.register(RealTimeBill)
+class RealTimeBillAdmin(admin.ModelAdmin):
+    list_display = ('meterid','totalconsumption','currentread', 'switch')
+ 
+ 
+@admin.register(ClientInfo)  
+class CLientInforAdmin(admin.ModelAdmin):
+    list_display = ('user','meterid','fullname')
+    
+    @admin.display(description='Name')
+    def fullname(self,obj):
+        return ("%s %s" % (obj.firstname, obj.lastname)).upper()
