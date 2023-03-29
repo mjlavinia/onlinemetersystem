2f81e11c6fc770403ec2b7559ef89258882071b1
joy <you@example.com>
2023-03-29 15:20:32 +0800

    deployment changes 03/29

--- OnlineMeterSystem/settings.py
+++ OnlineMeterSystem/settings.py
@@ -134,15 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATIC_ROOT =  '/static/'
 
-    STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static/'),)
-    
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
  
 # Default primary key field type
---
+++ staticfiles/assets/js/client/singlepage.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    uri = 'http://127.0.0.1:8000/api/getmeter?id=' + document.getElementById('clientid').innerHTML
+    function fetchValue() {
+        fetch(uri)
+          .then(response => response.json())
+          .then(data => {
+            console.log(data)
+            // Update the value element with the new value
+            document.getElementById('currentread').innerHTML = data.currentread
+            document.getElementById('total').innerHTML = data.total
+          })
+          .catch(error => {
+            console.error('Error fetching value:', error);
+          });
+      }
+      
+      // Call the fetchValue function every second
+      setInterval(fetchValue, 1000);
+});
